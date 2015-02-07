27e1748e5fc09cbd79c13d1fcf40d0b3b0218279
amandapickering <pickering.amanda@gmail.com>
2015-02-07 17:47:19 -0500

    pep 8 and README fixes

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A Python script (person_bot.py) that uses the Twython Twitter API wrapper to read tweets from a user's timeline and post Markov-generated tweets to a bot account, a Markov text generator (markov.py) to generate tweets, and a shell script (setup_heroku.sh) to get the bot automatically tweeting on Heroku.
 
-# usage
+## usage
 
 If you just want to make a single "ebook" account for yourself or a friend, follow these instructions. For a more detailed tutorial on making Twitter bots using this repo, read [this blog post](http://programmingforwitches.tumblr.com/post/110169568366/ebook-your-friends).
 
@@ -11,6 +11,6 @@ If you just want to make a single "ebook" account for yourself or a friend, foll
 3. Setup a new Twitter account for your bot
 4. Go to [apps.twitter.com](https://apps.twitter.com/), create a new application, and grab its API credentials
 5. Set your API credentials as environment variables (APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET). You can then check out some sample tweets from your bot in the Python repl by importing person_bot and running: ```generate_status(get_tweets(user))```
-6. run ```source setup_heroku.sh``` and follow the prompts (make sure you have git installed and a Heroku account)
+6. ```source setup_heroku.sh``` and follow the prompts to get your bot tweeting automatically with Heroku (make sure you have git installed and a Heroku account)
 
 Have fun (◡ ‿ ◡ ✿)
--- person_bot.py
+++ person_bot.py
@@ -2,7 +2,8 @@ from twython import Twython, TwythonError
 from markov import MarkovGenerator, twitter_tokenize
 from math import log
 from random import random
-import os, time
+import os
+import time
 
 APP_KEY = os.getenv('APP_KEY')
 APP_SECRET = os.getenv('APP_SECRET')
@@ -10,23 +11,27 @@ OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
 OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET')
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-user = '' # add twitter handle of user you want to ebook, edit!
+user = ''  # add twitter handle of user you want to ebook, edit!
 
 '''time between tweets in seconds
 set a minimum because we don't want near-simultaneous tweets'''
 minimum_interval = 300
 average_interval = 7200
 
+
 def get_tweets(username):
     '''(str) -> list of tweets,
     fetches last 200 tweets from specified user'''
     try:
-        user_timeline = twitter.get_user_timeline(screen_name=username, count=200, include_rts=False, exclude_replies=True)
-        tweets = [user_timeline[i]['text'] for i in range(len(user_timeline) - 1)]
+        user_timeline = twitter.get_user_timeline(
+            screen_name=username, count=200, include_rts=False, exclude_replies=True)
+        tweets = [user_timeline[i]['text']
+                  for i in range(len(user_timeline) - 1)]
         return tweets
     except TwythonError as e:
         print e
         return e
+
 
 def generate_status(tweet_list):
     '''(list) -> str,
@@ -40,6 +45,7 @@ def generate_status(tweet_list):
         print e
         pass
 
+
 def ebook():
     '''posts generated status to twitter'''
     status = generate_status(get_tweets(user))
@@ -47,11 +53,11 @@ def ebook():
         twitter.update_status(status=status)
         print time.strftime('[%y-%m-%dT%H:%M:%S] {}').format(status)
         time.sleep(minimum_interval -
-            log(random()) * (average_interval-minimum_interval))
+                   log(random()) * (average_interval - minimum_interval))
     except TwythonError as e:
         print e
         pass
 
 if __name__ == '__main__':
-    while  True:
+    while True:
         ebook()
